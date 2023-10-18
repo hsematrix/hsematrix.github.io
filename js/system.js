ec6bd6b37c65e0f697681d94a33f6e51324bafc6
hsematrix <hsematrix23@gmail.com>
2023-10-18 23:39:25 +0300

    ranks added

--- js/system.js
+++ js/system.js
@@ -1,4 +1,7 @@
 let det = document.getElementsByClassName("det")[0]
+let rank = document.getElementsByClassName("rank")
+rank[0].value='3'
+rank[1].value='3'
 detBool=true
 det.addEventListener('click', () => {
  	if(detBool==true){
@@ -14,3 +17,27 @@ det.addEventListener('click', () => {
  		setTimeout(function(){det.innerHTML=`det`}, 100)
  	}
  })
+
+rank[0].addEventListener('change',function(){
+	string=``
+	for (let i = 0; i < parseInt(rank[0].value); i++){
+		string+='<div class="line">'
+		for (let j = 0; j < parseInt(rank[1].value); j++){
+			string+=`<input type="" class="num">`
+		}
+		string+='</div>'
+	}
+	document.getElementsByClassName('matrix')[0].innerHTML=string
+})
+rank[1].addEventListener('change',function(){
+	string=``
+	for (let i = 0; i < parseInt(rank[0].value); i++){
+		string+='<div class="line">'
+		for (let j = 0; j < parseInt(rank[1].value); j++){
+			string+='<input type="" class="num">'
+		}
+		string+='</div>'
+	}
+	document.getElementsByClassName('matrix')[0].innerHTML=string
+})
+
