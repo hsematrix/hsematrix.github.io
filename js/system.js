0648461dc612d8fb1419e6a609461e4919412a78
hsematrix <xmik2022@gmail.com>
2023-10-19 20:45:17 +0300

    determinant and fixed bugs

--- js/system.js
+++ js/system.js
@@ -20,6 +20,42 @@ document.addEventListener('keydown', function(event) {
 rank[0].value='3'
 rank[1].value='3'
 detBool=true
+
+function toTop() {
+	document.body.style.overflow='hidden'
+	const duration = 150
+	const scrollStep = 10
+	const steps = Math.ceil(duration / scrollStep)
+	const scrollAmount = -window.scrollY / steps
+	function step() {
+	    if (window.scrollY > 0) {
+	    	window.scrollBy(0, scrollAmount)
+	    	requestAnimationFrame(step)
+	    }
+	}
+	step()
+}
+
+
+function toBot() {
+	document.body.style.overflow='auto'
+	const duration = 150
+  	const scrollHeight = document.body.scrollHeight
+	const windowHeight = window.innerHeight
+	const scrollStep = 10
+	const steps = Math.ceil(duration / scrollStep)
+	const scrollAmount = (scrollHeight - windowHeight) / steps
+	function step() {
+    if (window.scrollY + windowHeight < scrollHeight) {
+    	window.scrollBy(0, scrollAmount)
+    	requestAnimationFrame(step)
+    }
+	}
+	step()
+}
+
+toTop()
+
 document.body.style.overflow='hidden'
 det.addEventListener('click', () => {
  	if(detBool==true){
@@ -35,6 +71,7 @@ det.addEventListener('click', () => {
  			}
  		})
  		if(flag==true){
+ 			setTimeout(function(){toBot()}, 500)
 	 		detBool=false
 	 		det.style.transform='rotateX(180deg)'
 	 		det.style.backgroundColor='rgba(256,256,256,0.05)'
@@ -42,6 +79,7 @@ det.addEventListener('click', () => {
  		}
  	}
  	else if(detBool==false){
+ 		setTimeout(function(){toTop()}, 500)
  		detBool=true
  		det.style.transform='rotateX(0deg)'
  		det.style.backgroundColor='rgba(186,234,231,0.3)'
@@ -62,23 +100,65 @@ det.addEventListener('click', () => {
  })
 Array.from(document.getElementsByClassName('num')).forEach(function(item, index, array){
 		item.addEventListener('input', function(){
+			setTimeout(function(){
+				if(detBool==false){
+				toTop()
+		 		setTimeout(function(){toTop()}, 500)
+		 		detBool=true
+		 		det.style.transform='rotateX(0deg)'
+		 		det.style.backgroundColor='rgba(186,234,231,0.3)'
+		 		setTimeout(function(){det.innerHTML=`det`}, 100)
+		 	}
+			}, 500)
 			let inputValue = item.value
 			let numericValue = inputValue.replace(/[^0-9.-]/g, '')
   			item.value = numericValue
 		})	
 	})
 rank[0].addEventListener('change',function(){
+	setTimeout(function(){
+		toTop()
+		if(detBool==false){
+ 		setTimeout(function(){toTop()}, 500)
+ 		detBool=true
+ 		det.style.transform='rotateX(0deg)'
+ 		det.style.backgroundColor='rgba(186,234,231,0.3)'
+ 		numList=Array.from(document.getElementsByClassName('num'))
+ 		numList.forEach(function(item, index, array){
+ 			item.style.transform='rotateX(-180deg)'
+ 			setTimeout(function(){
+ 				item.value=''
+ 			}, 100)
+ 			setTimeout(function(){
+ 				item.style.transitionDuration='0s'
+ 				item.style.transform='rotateX(0deg)'
+ 				setTimeout(function(){item.style.transitionDuration='0.5s'}, 50)
+ 			}, 500)
+ 		})
+ 		setTimeout(function(){det.innerHTML=`det`}, 100)
+ 	}
+	}, 500)
 	string=``
 	for (let i = 0; i < parseInt(rank[0].value); i++){
 		string+='<div class="line">'
 		for (let j = 0; j < parseInt(rank[1].value); j++){
-			string+=`<input type="text" class="num">`
+			string+=`<input type="text" class="num" inputmode="numeric">`
 		}
 		string+='</div>'
 	}
 	document.getElementsByClassName('matrix')[0].innerHTML=string
 	Array.from(document.getElementsByClassName('num')).forEach(function(item, index, array){
 		item.addEventListener('input', function(){
+			setTimeout(function(){
+				if(detBool==false){
+				toTop()
+		 		setTimeout(function(){toTop()}, 500)
+		 		detBool=true
+		 		det.style.transform='rotateX(0deg)'
+		 		det.style.backgroundColor='rgba(186,234,231,0.3)'
+		 		setTimeout(function(){det.innerHTML=`det`}, 100)
+		 	}
+			}, 500)
 			let inputValue = item.value
 			let numericValue = inputValue.replace(/[^0-9.-]/g, '')
   			item.value = numericValue
@@ -86,17 +166,49 @@ rank[0].addEventListener('change',function(){
 	})
 })
 rank[1].addEventListener('change',function(){
+	setTimeout(function(){
+		toTop()
+		if(detBool==false){
+ 		setTimeout(function(){toTop()}, 500)
+ 		detBool=true
+ 		det.style.transform='rotateX(0deg)'
+ 		det.style.backgroundColor='rgba(186,234,231,0.3)'
+ 		numList=Array.from(document.getElementsByClassName('num'))
+ 		numList.forEach(function(item, index, array){
+ 			item.style.transform='rotateX(-180deg)'
+ 			setTimeout(function(){
+ 				item.value=''
+ 			}, 100)
+ 			setTimeout(function(){
+ 				item.style.transitionDuration='0s'
+ 				item.style.transform='rotateX(0deg)'
+ 				setTimeout(function(){item.style.transitionDuration='0.5s'}, 50)
+ 			}, 500)
+ 		})
+ 		setTimeout(function(){det.innerHTML=`det`}, 100)
+ 	}
+	}, 500)
 	string=``
 	for (let i = 0; i < parseInt(rank[0].value); i++){
 		string+='<div class="line">'
 		for (let j = 0; j < parseInt(rank[1].value); j++){
-			string+='<input type="text" class="num">'
+			string+='<input type="text" class="num" inputmode="numeric">'
 		}
 		string+='</div>'
 	}
 	document.getElementsByClassName('matrix')[0].innerHTML=string
 	Array.from(document.getElementsByClassName('num')).forEach(function(item, index, array){
 		item.addEventListener('input', function(){
+			setTimeout(function(){
+				if(detBool==false){
+				toTop()
+		 		setTimeout(function(){toTop()}, 500)
+		 		detBool=true
+		 		det.style.transform='rotateX(0deg)'
+		 		det.style.backgroundColor='rgba(186,234,231,0.3)'
+		 		setTimeout(function(){det.innerHTML=`det`}, 100)
+		 	}
+			}, 500)
 			let inputValue = item.value
 			let numericValue = inputValue.replace(/[^0-9.-]/g, '')
   			item.value = numericValue
