3339d81ff6c732bb6f2a6a4cb3543b6e3efadb20
hsematrix <hsematrix23@gmail.com>
2023-10-19 16:23:43 +0300

    base-commit

--- js/system.js
+++ js/system.js
@@ -1,5 +1,12 @@
 let det = document.getElementsByClassName("det")[0]
 let rank = document.getElementsByClassName("rank")
+
+// - - - Здесь функция по подсчету определителя
+// - - -
+
+// - - - Здесь по рангам
+// - - -
+
 rank[0].value='3'
 rank[1].value='3'
 detBool=true
@@ -8,17 +15,29 @@ det.addEventListener('click', () => {
  		detBool=false
  		det.style.transform='rotateX(180deg)'
  		det.style.backgroundColor='rgba(256,256,256,0.05)'
- 		setTimeout(function(){det.innerHTML=`<span style="transform:rotateX(180deg);color:rgba(144,235,230,0.7);">test</span>`}, 100)
+ 		setTimeout(function(){det.innerHTML=`<span style="transform:rotateX(180deg);color:rgba(144,235,230,0.7);">reset</span>`}, 100)
  	}
  	else if(detBool==false){
  		detBool=true
  		det.style.transform='rotateX(0deg)'
  		det.style.backgroundColor='rgba(186,234,231,0.3)'
+ 		numList=Array.from(document.getElementsByClassName('num'))
+ 		numList.forEach(function(item, index, array){
+ 			item.style.transform='rotateX(-180deg)'
+ 			setTimeout(function(){
+ 				item.value=''
+ 			}, 100)
+ 			setTimeout(function(){
+ 				item.style.transitionDuration='0s'
+ 				item.style.transform='rotateX(0deg)'
+ 				setTimeout(function(){item.style.transitionDuration='0.5s'}, 50)
+ 			}, 500)
+ 		})
  		setTimeout(function(){det.innerHTML=`det`}, 100)
  	}
  })
 
-rank[0].addEventListener('change',function(){
+rank[0].addEventListener('change',function(){	
 	string=``
 	for (let i = 0; i < parseInt(rank[0].value); i++){
 		string+='<div class="line">'
