07a7978110904a1643cca53060aaaecaffbd3e41
Андрей Степанов <therain3164@yandex.ru>
2023-10-21 20:22:56 +0300

    Fixed determinant for 1x1

--- js/system.js
+++ js/system.js
@@ -137,6 +137,9 @@ function detr(matrix) {
     if (isSquare(matrix) === "") {
         return "none";
     }
+	if (matrix.length === 1) {
+		return matrix;
+	}
     if (matrix.length === 2 && matrix[0].length === 2) {
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0];
     }
