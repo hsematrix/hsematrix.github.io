c0849031aaf9be771d60cce60ff653a65b43b010
Shon Qwezon <shonzbso@gmail.com>
2023-10-21 21:35:42 +0300

    Fix rank error

--- js/rank.js
+++ js/rank.js
@@ -12,8 +12,14 @@ function swap_columns(matrix, col1, col2, row) {
         matrix[i][col1] = matrix[i][col2];
 }
 
+function makeSquare(matrix, R, C) {
+    empty_row = new Array(C).fill(0);
+    for (let i = 0; i < C - R; i++) matrix.push(empty_row);
+}
+
 function get_rank(matrix, R, C) {
-    let rank = Math.min(R, C);
+    let rank = C;
+    if (R < C) makeSquare(matrix, R, C);
     for (let row = 0; row < rank; row++) {
         // Элемент на главной диагонали != 0
         if (matrix[row][row]) {
@@ -40,7 +46,6 @@ function get_rank(matrix, R, C) {
                 swap_columns(matrix, row, rank, R);
             }
             row--;
-            // display(matrix);
         }
     }
     return rank;
